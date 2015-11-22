f410719f2066dc2eba8f97882ee76a42f5b02fb9
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-11-22 06:04:40 -0800

    yada-yada

--- t2/main.tf
+++ t2/main.tf
@@ -14,7 +14,6 @@ resource "aws_security_group" "cheftest" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   tags {
 	Name = "cheftest"
   }
@@ -24,10 +23,128 @@ resource "aws_instance" "chef" {
   ami = "${lookup(var.amis, var.region)}"
   instance_type = "t1.micro"
   key_name = "ephemeral-test"
-  volume_size = 100
   security_groups = ["${aws_security_group.cheftest.name}"]
-
+  root_block_device {
+    volume_size = "100"
+  }
   tags {
     Name = "cheftest"
   }
 }
+
+
+
+
+
+
+
+resource "aws_route53_zone" "streambox-com" {
+    name = "streambox.com"
+#     lifecycle {
+#         prevent_destroy = true
+#     }
+}
+
+resource "aws_route53_record" "streambox-com_a" {
+    zone_id = "${aws_route53_zone.streambox-com.zone_id}"
+    name = "streambox.com"
+    type = "A"
+    ttl = "300"
+    records = ["50.18.251.59"]
+}
+
+resource "aws_route53_record" "streambox-com_cname_chef" {
+    zone_id = "${aws_route53_zone.streambox-com.zone_id}"
+    name = "chef"
+    type = "CNAME"
+    ttl = "300"
+   records = ["${aws_instance.chef.public_dns}"]
+#    records = ["chef.streambox.com"]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+# 
+# 
+# resource "aws_route53_record" "chef_dns" {
+#    zone_id = "streambox.com"
+#    name = "chef.streambox.com"
+#    type = "A"
+#    ttl = "300"
+# #   records = ["${aws_instance.public_ip}"]
+#    records = ["127.0.0.1"]
+# }
+# 
+# 
+
+
+# 
+# resource "aws_route53_zone" "chef" {
+#   name = "chef.streambox.com"
+# }
+# 
+# resource "aws_route53_record" "chef" {
+#   zone_id = "${aws_route53_zone.chef.zone_id}"
+#   name = "chef.chef.streambox.com"
+#   type = "CNAME"
+#   ttl = "300"
+#   records = ["${aws_instance.chef.public_dns}"]
+# }
+# 
+# 
+# 
+
+
+
+
+# CNAME with DNS name chef.streambox.com. is not permitted at apex in zone chef.streambox.com.
+# CNAME with DNS name . is not permitted at apex in zone 
+# 
+# resource "aws_route53_zone" "primary" {
+#   name = "streambox.com"
+# }
+# 
+# # resource "aws_route53_record" "root" {
+# #     zone_id = "${aws_route53_zone.primary.zone_id}"
+# #     name = "streambox.com"
+# #     type = "A"
+# #     ttl = "300"
+# #   records = ["${aws_instance.chef.public_ip}"]
+# # }
+# 
+# # 
+# # # terraform cname
+# # 
+# resource "aws_route53_record" "chef" {
+#    zone_id = "streambox.com"
+#    name = "chef.streambox.com"
+#    type = "CNAME"
+#    ttl = "300"
+#    records = ["${aws_instance.chef.public_dns}"]
+# }
+# 
+# # 
+# 
+# 
+# resource "aws_route53_zone" "streambox" {
+#   name = "streambox.com"
+# }
+# 
+# resource "aws_route53_record" "chef" {
+#   zone_id = "${aws_route53_zone.streambox.zone_id}"
+#   domain
+#   name = ".streambox.com"
+#   type = "CNAME"
+#   ttl = "300"
+#   records = ["${aws_instance.streambox.ipv4_address}"]
+# }
+# 
