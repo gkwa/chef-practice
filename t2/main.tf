1b39aebb4010bb00eb03795a4e0bf411026bfe48
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-12-13 10:42:27 -0800

    Chef uses port 443, open it

--- t2/main.tf
+++ t2/main.tf
@@ -55,6 +55,12 @@ resource "aws_security_group" "chef" {
 	protocol = "tcp"
 	cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+	from_port = 443
+	to_port = 443
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
   tags {
 	Name = "chef"
   }
