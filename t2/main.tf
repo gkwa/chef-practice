6d086352a80657883f0155f43320f1cbed582760
Taylor Monacelli <taylormonacelli@gmail.com>
2017-09-04 19:30:59 -0700

    Whitespace

--- t2/main.tf
+++ t2/main.tf
@@ -19,53 +19,53 @@ resource "aws_instance" "chef" {
   security_groups = ["${aws_security_group.chef.name}"]
   monitoring = true
   root_block_device {
-	volume_size = "100"
+    volume_size = "100"
   }
 
   tags {
-	Name = "chef"
+    Name = "chef"
   }
 
   connection {
-	agent = false
-	user = "ubuntu"
-	key_file = "~/.ssh/ephemeral-test.pem"
+    agent = false
+    user = "ubuntu"
+    key_file = "~/.ssh/ephemeral-test.pem"
   }
 
   provisioner "file" {
-	source = "script.sh"
-	destination = "/tmp/script.sh"
+    source = "script.sh"
+    destination = "/tmp/script.sh"
   }
 
   provisioner "file" {
-	source = "sethosts.sh"
-	destination = "/tmp/sethosts.sh"
+    source = "sethosts.sh"
+    destination = "/tmp/sethosts.sh"
   }
 
   provisioner "file" {
-	source = "sethostname.sh"
-	destination = "/tmp/sethostname.sh"
+    source = "sethostname.sh"
+    destination = "/tmp/sethostname.sh"
   }
 
   provisioner "file" {
-	source = "installemacs.sh"
-	destination = "/tmp/installemacs.sh"
+    source = "installemacs.sh"
+    destination = "/tmp/installemacs.sh"
   }
 
   provisioner "file" {
-	source = "s1.sh"
-	destination = "/tmp/s1.sh"
+    source = "s1.sh"
+    destination = "/tmp/s1.sh"
   }
 
   provisioner "remote-exec" {
-	inline = [
-	  "sudo cp -R /home/ubuntu/.ssh /root" # enables ssh root@chef
-	  ,"sudo sh /tmp/sethostname.sh"
-	  ,"sudo sh /tmp/sethosts.sh"
-	  ,"sudo service hostname restart"
-	  ,"sudo nohup sh -x /tmp/s1.sh &"
-	  ,"sleep 3" # without this, the nohup doesn't always run (not sure why)
-	]
+    inline = [
+      "sudo cp -R /home/ubuntu/.ssh /root" # enables ssh root@chef
+      ,"sudo sh /tmp/sethostname.sh"
+      ,"sudo sh /tmp/sethosts.sh"
+      ,"sudo service hostname restart"
+      ,"sudo nohup sh -x /tmp/s1.sh &"
+      ,"sleep 3" # without this, the nohup doesn't always run (not sure why)
+  ]
   }
 }
 
@@ -74,19 +74,19 @@ resource "aws_security_group" "chef" {
   description = "Allow ssh inbound traffic from everywhere"
 
   ingress {
-	from_port = 22
-	to_port = 22
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-	from_port = 443
-	to_port = 443
-	protocol = "tcp"
-	cidr_blocks = ["0.0.0.0/0"]
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
   }
   tags {
-	Name = "chef"
+  Name = "chef"
   }
 }
 
